26870be81da58edc5c97c76177dfc1cab1172d7a
PatoGordo <icariusv@gmail.com>
2021-11-01 01:58:56 +0000

    adding react-navigation and styled-components

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import Home from "./views/Home";
+import About from "./views/About";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export type RootStackParamList = {
+  Home: undefined;
+  About: undefined;
+};
---
+++ views/About.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Button } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+
+type AboutScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "About"
+>;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  color: #000;
+  text-align: center;
+  font-size: 16px;
+`;
+
+export default ({ navigation }: { navigation: AboutScreenNavigationProp }) => (
+  <Container>
+    <Title>Hello World from About</Title>
+    <Button onPress={() => navigation.navigate("Home")} title="Go About Page" />
+  </Container>
+);
